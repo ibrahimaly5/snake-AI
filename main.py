6d24b867d1bdee7a1482864a6ef6f8a0d6817ca8
Ibrahim Aly <ialy@ualberta.ca>
2018-12-02 13:03:46 -0700

    main menu in works

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 from src.window import GameBoard
 import random
 from time import sleep
+from tkinter import *
+import sys
+
+
+def callback1(root):
+	root.destroy()
+	sys.exit(-1)
+
+def callback2(root):
+	root.destroy()
+	game_class = GameBoard()
+
+	game_class.run_game()
+
+def callback3(root):	
+	root.destroy()
+
+
 
 def main():
-    plot_game = True
 
-    game_class = GameBoard()
-    if plot_game:
-        game_class.run_game()
+	root = Tk()
+
+	frame = Frame(root, width=600, height=400, bg = "black")
+	
+	humanButton = Button(root,command = callback2(root), text="Single Player", background = "blue", highlightthickness=1)
+	humanButton.place(x=300, y=150)
+
+	aiButton = Button(root,command = callback3(root), text="AI", background = "blue", highlightthickness=1)
+	aiButton.place(x=300, y=200)
+
+	quitButton = Button(root,command = callback1(root), text="Quit", background = "blue", highlightthickness=1)
+	quitButton.place(x=300, y=250)
+
+	frame.pack()
+
+	root.mainloop()
+
+
 
 
 
 if __name__ == '__main__':
-    while True:
-        main()
-        sleep(5)
+	while True:
+		main()
--- src/trials.py
+++ src/trials.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import sys
 
 root = Tk()
 
@@ -9,9 +10,47 @@ def callback(event):
     frame.focus_set()
     print("clicked at", event.x, event.y)
 
-frame = Frame(root, width=100, height=100)
+def callback2():
+	root.destroy()
+
+frame = Frame(root, width=600, height=400)
 frame.bind("<Key>", key)
 frame.bind("<Button-1>", callback)
+
+quitButton = Button(root,command = callback2, text="Exit", background = "blue", highlightthickness=1)
+quitButton.place(x=550, y=300)
+
 frame.pack()
 
 root.mainloop()
+
+
+'''
+class Game(tkinter.Tk):
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+
+
+	frame = Frame(self, width=600, height=400)
+	frame.bind("<Key>", key)
+	frame.bind("<Button-1>", callback)
+
+	quitButton = Button(self,command = callback2, text="Exit", background = "blue", highlightthickness=1)
+	quitButton.place(x=550, y=300)
+
+	frame.pack()
+
+	self.mainloop()
+
+
+def main():
+
+    snake_game = Game()
+
+
+if __name__ == '__main__':
+    while True:
+        main()
+'''
+
+
